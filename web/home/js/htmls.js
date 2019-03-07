6b3e38a4ea5a4d35c8b9394ef8d758feae628423
duyiming <nahso4@outlook.com>
2019-03-07 15:00:52 +0800

    刚加上首页，还没改数据

---
+++ web/home/js/htmls.js
@@ -0,0 +1,51 @@
+function load_home1() {
+ 	var one=document.getElementById("1");
+ 	one.style.display = 'block';
+ 	var two=document.getElementById("2");
+ 	two.style.display = 'none';
+ 	var two=document.getElementById("3");
+ 	two.style.display = 'none';
+ 	var two=document.getElementById("4");
+ 	two.style.display = 'none';
+ 	var css=document.getElementById("css1");
+ 	css.href="../首页/css/style.css"
+}
+
+function load_home2() {
+ 	var one=document.getElementById("1");
+ 	one.style.display = 'none';
+ 	var two=document.getElementById("2");
+ 	two.style.display = 'block';
+ 	var two=document.getElementById("3");
+ 	two.style.display = 'none';
+ 	var two=document.getElementById("4");
+ 	two.style.display = 'none';
+ 	var css=document.getElementById("css1");
+ 	css.href="../我的收藏/css/style.css"
+}
+
+function load_home3() {
+ 	var one=document.getElementById("1");
+ 	one.style.display = 'none';
+ 	var two=document.getElementById("2");
+ 	two.style.display = 'none';
+ 	var two=document.getElementById("3");
+ 	two.style.display = 'block';
+ 	var two=document.getElementById("4");
+ 	two.style.display = 'none';
+ 	var css=document.getElementById("css1");
+ 	css.href="../我的点赞/css/style.css"
+}
+
+function load_home4() {
+ 	var one=document.getElementById("1");
+ 	one.style.display = 'none';
+ 	var two=document.getElementById("2");
+ 	two.style.display = 'none';
+ 	var two=document.getElementById("3");
+ 	two.style.display = 'none';
+ 	var two=document.getElementById("4");
+ 	two.style.display = 'block';
+ 	var css=document.getElementById("css1");
+ 	css.href="../我的评论（发出）/css/style.css"
+}
